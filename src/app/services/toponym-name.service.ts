0a3e372d1d61934fe16d93fcb89d52a4ba6d4dec
7herion <1037762715.mz@gmail.com>
2021-08-15 14:55:21 +0200

    :adhesive_bandage: extract name from toponym

--- src/app/services/toponym-name.service.ts
+++ src/app/services/toponym-name.service.ts
@@ -8,21 +8,23 @@ export class ToponymNameService {
   constructor() { }
 
   public extract(data: string): string {
-    // TODO: extract name from toponym
-
     if (data.indexOf('-') != -1) {
       data = data.slice(0, (data.indexOf('-') - 1));
-
-      if (data.indexOf(' e ') != -1) {
-        data = data.slice(0, data.indexOf(' e '));
-      }
-
     } else {
-      data = data.substring(data.indexOf(" ") + 1);
+      data = data.substring(data.indexOf(' ') + 1);
 
       let re = new RegExp('^[a-z].* ');
       if (re.test(data)) {
         data = data.substring(data.indexOf(" ") + 1);
+      }
+    }
+
+    if (data.indexOf(' e ') != - 1) {
+      let second_name = data.slice(data.indexOf(' e ') + 3);
+      data = data.slice(0, data.indexOf(' e '));
+
+      if (data.indexOf(' ') == - 1) {
+        data = second_name;
       }
     }
 
