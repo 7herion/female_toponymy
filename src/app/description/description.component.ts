90e0371867a159af9cd2a9a087f46a0a934eb587
7herion <1037762715.mz@gmail.com>
2021-09-25 13:51:53 +0200

    Added getImage in wikipedia query service

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -12,8 +12,10 @@ export class DescriptionComponent implements OnInit {
 
   public name!: string;
   public dataFound: boolean = false;
+  public imageFound: boolean = false;
 
   public descr!: string;
+  public imageSource: string = '';
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private readonly data: string,
@@ -31,6 +33,14 @@ export class DescriptionComponent implements OnInit {
         this.dataFound = true;
       } else {
         this.descr = '<i>Nessun dato trovato su Wikipedia.</i>';
+      }
+    });
+
+    this.wikiQuery.getImage(this.name).subscribe(res => {
+      let pageNr = +Object.getOwnPropertyNames(res.query.pages)[0];
+      if (pageNr != -1 && res.query.pages[pageNr].original) {
+        this.imageSource = res.query.pages[pageNr].original.source;
+        this.imageFound = true;
       }
     });
   }
--- src/app/services/wikipedia-query.service.ts
+++ src/app/services/wikipedia-query.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 const WIKIPEDIA_API_ENDPOINT = `https://it.wikipedia.org/w/api.php`;
 const WIKIPEDIA_API_OPTIONS = `?format=json&action=query&prop=extracts&redirects=1&origin=*`;
-
+const WIKIPEDIA_API_IMAGE_OPTIONS = `?format=json&action=query&prop=pageimages&piprop=original&origin=*`;
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,10 @@ export class WikipediaQueryService {
 
   public getInfo(name: string) {
     return this.http.get<any>(`${WIKIPEDIA_API_ENDPOINT}${WIKIPEDIA_API_OPTIONS}&titles=${name}`);
+  }
+
+  public getImage(name: string) {
+    return this.http.get<any>(`${WIKIPEDIA_API_ENDPOINT}${WIKIPEDIA_API_IMAGE_OPTIONS}&titles=${name}`);
   }
 
   constructor(
